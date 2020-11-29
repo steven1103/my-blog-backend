e8eb7930faa08c66f8d158ea142fe2515681d64f
steven1103 <steven1103@naver.com>
2020-11-29 21:39:14 +0900

    explore

---
+++ src/api/Post/explorePost/explorePost.js
@@ -0,0 +1,14 @@
+import { prisma } from "../../../../generated/prisma-client"
+
+export default {
+    Query:{
+        explore:(_,args) => {
+            return prisma.posts({
+                where:{
+                title_contains:""
+            },
+            orderBy:"createdAt_DESC"
+        })
+        }
+    }
+}
